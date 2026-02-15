e8dd05a1c67d492442ba4eaea9548d0fd43e4ff5
huykeang <sreytouch3456789@email.com>
2026-02-15 15:48:56 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+// import 
+
+function Navbar(){
+    return(
+        <>
+            <nav className="navbar flex items-center justify-center">
+                <h1>Hazel</h1>
+                <div className="menu">
+                    <h1>Home</h1>
+                    <h1>About</h1>
+                    <h1>Skills</h1>
+                    <h1>Projects</h1>
+                    <h1>Contact</h1>
+                </div>
+            </nav>
+        </>
+    )
+};
+
+export default Navbar;
