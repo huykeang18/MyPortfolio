048bcd797bfc14305c591bb3b973cad04a422b53
huykeang <sreytouch3456789@email.com>
2026-02-16 23:45:08 +0700

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,42 @@
-// import 
 
-function Navbar(){
-    return(
-        <>
-            <nav className="navbar container-cus flex items-center justify-between h-22 text-xl bg-sky-200 fixed">
-                <h1>Hazel</h1>
-                <div className="menu flex gap-12">
-                    <h1>Home</h1>
-                    <h1>About</h1>
-                    <h1>Skills</h1>
-                    <h1>Projects</h1>
-                    <h1>Contact</h1>
-                </div>
-            </nav>
-        </>
-    )
-};
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar h-22 flex items-center bg-sky-200 fixed w-full backdrop-blur-lg shadow-md">
+        <div className="container-cus flex items-center justify-between">
+          <h1 className="lg:text-xl md:text-base text-sm">Hazel</h1>
+
+          <ul className="menu flex gap-12">
+            <li>
+              <a href="#home" className="lg:text-xl md:text-base text-sm">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="lg:text-xl md:text-base text-sm">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#skills" className="lg:text-xl md:text-base text-sm">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#projects" className="lg:text-xl md:text-base text-sm">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="lg:text-xl md:text-base text-sm">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
 
 export default Navbar;
